1627fafba777ed5f3d68c3a3884dcba19c930258
AlinaValyarova <113130561+AlinaValyarova@users.noreply.github.com>
2023-05-12 22:23:14 +0300

    Добавьте файлы проекта.

---
+++ TotalComander/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿
+namespace TotalComander
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(703, 72);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(422, 212);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDown_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(700, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Папки:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(700, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Файлы:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(703, 340);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox2.Size = new System.Drawing.Size(422, 308);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(254, 311);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Файлы:";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(257, 340);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox3.Size = new System.Drawing.Size(422, 308);
+            this.listBox3.TabIndex = 6;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(254, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Папки:";
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 16;
+            this.listBox4.Location = new System.Drawing.Point(257, 72);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox4.Size = new System.Drawing.Size(422, 212);
+            this.listBox4.TabIndex = 4;
+            this.listBox4.SelectedIndexChanged += new System.EventHandler(this.listBox4_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(700, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(254, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "label6";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Location = new System.Drawing.Point(531, 204);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(340, 191);
+            this.panel1.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 42);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(323, 17);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Введите имя папки, которую вы хотите создать";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(334, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "-";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Создать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1126, 660);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TotalComander/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Diagnostics;
+
+
+
+namespace TotalComander
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            panel1.Visible = false;
+            ToolStripMenuItem first = new ToolStripMenuItem("Создать");
+            ToolStripMenuItem second = new ToolStripMenuItem("Удалить");
+            ToolStripMenuItem third = new ToolStripMenuItem("Переименовать");
+            ToolStripMenuItem fourth = new ToolStripMenuItem("Скопировать");
+            ToolStripMenuItem fith = new ToolStripMenuItem("Свойства");
+            // добавляем элементы в меню
+            contextMenuStrip1.Items.AddRange(new[] { first, second, third, fourth, fith});
+            // ассоциируем контекстное меню с текстовым полем
+            listBox1.ContextMenuStrip = contextMenuStrip1;
+            listBox1.ContextMenuStrip = contextMenuStrip1;
+            listBox1.ContextMenuStrip = contextMenuStrip1;
+            listBox1.ContextMenuStrip = contextMenuStrip1;
+            listBox1.ContextMenuStrip = contextMenuStrip1;
+            // устанавливаем обработчики событий для меню
+            first.Click += first_Click;
+            second.Click += second_Click;
+            third.Click += third_Click;
+            fourth.Click += fourth_Click;
+            fith.Click += fith_Click;
+
+            listBox1.ContextMenuStrip = contextMenuStrip1;
+        }
+
+
+        private void first_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+
+
+        }
+        private void second_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+    "Удалить папку?",
+    "Сообщение",
+    MessageBoxButtons.YesNo,
+    MessageBoxIcon.Information,
+    MessageBoxDefaultButton.Button1,
+    MessageBoxOptions.DefaultDesktopOnly);
+
+            if (result == DialogResult.Yes)
+            {
+                Directory.Delete(label5.Text);
+            }    
+        }
+        private void third_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void fourth_Click(object sender, EventArgs e)
+        {
+            //foreach (string dirPath in Directory.GetDirectories(SourcePath, "*", SearchOption.AllDirectories))
+            //    Directory.CreateDirectory(dirPath.Replace(SourcePath, DestinationPath));
+
+            ////Скопировать все файлы. И перезаписать(если такие существуют)
+            //foreach (string newPath in Directory.GetFiles(SourcePath, "*.*", SearchOption.AllDirectories))
+            //    File.Copy(newPath, newPath.Replace(SourcePath, DestinationPath), true);
+        }
+        private void fith_Click(object sender, EventArgs e)
+        {
+
+        }
+        public List<string> files = new List<string>();
+        public List<string> dirs = new List<string>();
+        public int i = 0;
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+                string path2 = listBox1.SelectedItem.ToString();
+                label5.Text = path2;
+
+                listBox1.Items.Clear();
+                listBox2.Items.Clear();
+
+                List<string> newfiles = new List<string>();
+                List<string> newdirs = new List<string>();
+
+                try
+                {
+                    files.Clear();
+                    files.AddRange(Directory.GetFiles(path2));
+                    dirs.Clear();
+                    dirs.AddRange(Directory.GetDirectories(path2));
+                }
+                catch (UnauthorizedAccessException a2)
+                {
+
+                }
+
+                foreach (string dir in dirs)
+                {
+                    listBox1.Items.Add(dir);
+                }
+
+
+                foreach (string file in files)
+                {
+
+                    listBox2.Items.Add(file);
+                }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+ 
+
+            var path = @"c:\";
+
+            label6.Text = path;
+            label5.Text = path;
+
+
+            try
+            {
+                files.AddRange(Directory.GetFiles(path));
+                dirs.AddRange(Directory.GetDirectories(path));
+            }
+            catch (UnauthorizedAccessException a)
+            {
+                
+            }
+
+            foreach (string dir in dirs)
+            {
+                listBox1.Items.Add(dir);
+                listBox4.Items.Add(dir);
+            }
+
+            foreach(string file in files)
+            {
+                listBox2.Items.Add(file);
+                listBox3.Items.Add(file);
+            }
+        }
+
+        private void listBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string path2 = listBox4.SelectedItem.ToString();
+            label5.Text = path2;
+
+            listBox4.Items.Clear();
+            listBox3.Items.Clear();
+
+            List<string> newfiles = new List<string>();
+            List<string> newdirs = new List<string>();
+
+            try
+            {
+                files.Clear();
+                files.AddRange(Directory.GetFiles(path2));
+                dirs.Clear();
+                dirs.AddRange(Directory.GetDirectories(path2));
+            }
+            catch (UnauthorizedAccessException a2)
+            {
+
+            }
+
+            foreach (string dir in dirs)
+            {
+                listBox4.Items.Add(dir);
+            }
+
+
+            foreach (string file in files)
+            {
+
+                listBox3.Items.Add(file);
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string path = listBox2.SelectedItem.ToString();
+            Process.Start(path);
+
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string path = listBox3.SelectedItem.ToString();
+            Process.Start(path);
+        }
+
+        private void listBox1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                label5.Text = listBox1.SelectedItem.ToString();
+                //select the item under the mouse pointer
+                listBox1.SelectionMode = SelectionMode.None;
+                i = listBox1.IndexFromPoint(e.Location);
+                if (i != -1)
+                {
+                    contextMenuStrip1.Show();
+                }
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "-")
+            {
+                string path = label5.Text + $"/{textBox1.Text}";
+                Directory.CreateDirectory(path);
+                MessageBox.Show("Yes");
+                panel1.Hide();
+            }
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("No");
+            }
+        }
+    }
+}
